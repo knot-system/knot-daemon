188503ec4bd54ff124dc993a6a20ed654abfd27e
maxhaesslein <git@maxhaesslein.de>
2023-02-21 11:48:43 +0100

    very basic cron feed handling

--- system/load_cron.php
+++ system/load_cron.php
@@ -28,6 +28,55 @@ if( $secret != $secret_option ) {
 	exit;
 }
 
-// TODO: cron stuff ...
+
+
+$active_feeds = []; // NOTE: these are all the active feeds of _all_ users on this system
+
+
+// TODO: add a filter parameter, to only refresh a specific user
+$userfolders_obj = new Folder( $postamt->abspath.'content/' );
+$userfolders = $userfolders_obj->get_subfolders();
+
+if( empty($userfolders) ) {
+	// no users exist yet
+	exit;
+}
+
+
+foreach( $userfolders as $userfolder ) {
+
+	// TODO: check, when the last user login was; if it was a long time, reduce the frequency of updates
+
+	$channels_obj = new Channels( $postamt, $userfolder['path'] );
+
+	$channels = $channels_obj->get( false, true );
+
+	if( empty($channels) ) continue;
+
+	foreach( $channels as $channel ) {
+
+		$feeds_obj = new Feeds( $channel );
+		$feeds = $feeds_obj->get( false, true );
+
+		if( empty($feeds) ) continue;
+
+		foreach( $feeds as $feed ) {
+			$active_feeds[] = $feed;
+		}
+
+	}
+
+}
+
+if( empty($active_feeds) ) exit;
+
+
+// TODO: build some type of queue, so we don't loop over _all_ active feeds every time, but split it up into multiple requests to cron
+
+foreach( $active_feeds as $active_feed ) {
+	$active_feed->refresh_posts();
+	$active_feed->cleanup_posts();
+}
+
 
 exit;
