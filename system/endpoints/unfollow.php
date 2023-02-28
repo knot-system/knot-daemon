21d702fb6aaddf129d4ae8e48e3d4baf0bde5b48
maxhaesslein <git@maxhaesslein.de>
2023-02-28 13:20:10 +0100

    unfollow: log additional information

--- system/endpoints/unfollow.php
+++ system/endpoints/unfollow.php
@@ -5,7 +5,7 @@ if( ! $postamt ) exit;
 $request_type = $postamt->route->get('request_type');
 
 if( $request_type != 'post' ) {
-	$postamt->error( 'invalid_request', 'only post accepted' );
+	$postamt->error( 'invalid_request', 'only post accepted', null, null, $request_type );
 }
 
 if( ! isset($_REQUEST['channel']) ) {
@@ -17,23 +17,23 @@ $channel_uid = $_REQUEST['channel'];
 $channel = $postamt->channels->get( $channel_uid, true );
 
 if( ! $channel ) {
-	$postamt->error( 'invalid_request', 'channel not found' );
+	$postamt->error( 'invalid_request', 'channel not found', null, null, $channel_uid );
 }
 
 $feeds = new Feeds( $channel );
 
 if( ! isset($_REQUEST['url']) ) {
-	$postamt->error( 'invalid_request', 'missing url' );
+	$postamt->error( 'invalid_request', 'missing url', null, null, $channel_uid, $channel );
 }
 
 $url = trim($_REQUEST['url']);
 
 if( ! $feeds->feed_exists( false, $url ) ) {
-	$postamt->error( 'invalid_request', 'this url does not exist in this channel' );
+	$postamt->error( 'invalid_request', 'this url does not exist in this channel', null, null, $url, $channel_uid, $channel );
 }
 
 if( ! $feeds->remove_feed( $url ) ) {
-	$postamt->error( 'internal_server_error', 'could not remove feed from channel', 500 );
+	$postamt->error( 'internal_server_error', 'could not remove feed from channel', 500, null, $url, $channel_uid, $channel );
 }
 
 exit;
