95fa829a24ffddbc01547db83ec5d3f7d434a56c
maxhaesslein <git@maxhaesslein.de>
2023-08-02 13:13:01 +0200

    added compatibility with homestead-control

---
+++ system/config_definitions.php
@@ -0,0 +1,14 @@
+<?php
+
+// these options are displayed in the 'homestead-control' module
+
+return [
+	'force_refresh_posts' => [
+		'type' => 'bool',
+		'description' => '',
+	],
+	'refresh_on_connect' => [
+		'type' => 'bool',
+		'description' => '',
+	],
+];
