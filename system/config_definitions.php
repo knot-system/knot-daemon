859bfe81df77ae010c1d389a742875b94c62fff6
maxhaesslein <git@maxhaesslein.de>
2023-08-02 13:22:33 +0200

    config definitions: added additional descriptions

--- system/config_definitions.php
+++ system/config_definitions.php
@@ -5,10 +5,10 @@
 return [
 	'force_refresh_posts' => [
 		'type' => 'bool',
-		'description' => '',
+		'description' => 'set this to <code>true</code> if you want to refresh all post content when refreshing feeds; or set to <code>false</code> if you want to skip all existing posts, even if they changed',
 	],
 	'refresh_on_connect' => [
 		'type' => 'bool',
-		'description' => '',
+		'description' => 'this will refresh all items of all feeds of a channel, if you call the <code>timeline</code> endpoint to get the feeds. set to <code>false</code> if you use a cronjob, to make the system faster, otherwise set to <code>true</code>',
 	],
 ];
